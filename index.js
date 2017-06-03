921b0b10932a3594acae81ed7f9f0bbd7d00cca2
Tuguldur Baigalmaa <philoniare@gmail.com>
2017-06-03 04:12:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("Failed to produce the correct result", "The laws of the universe have failed.");
+console.warn("Just a heads up, you're about to launch a nuclear missile");
+console.log("JS Foundations");
